d08cd1d8e0e0f12e50704445ea3189c4a8be70bc
Jeremy V Siahaan <44839061+Makro62@users.noreply.github.com>
2025-06-08 06:54:11 +0700

    Create LoginKeywords.py

---
+++ Web/keywords/LoginKeywords.py
@@ -0,0 +1,30 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.by import By
+import time
+
+class LoginKeywords:
+    def __init__(self):
+        self.driver = None
+
+    def open_browser(self):
+        self.driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+        self.driver.get("https://www.saucedemo.com")
+        self.driver.maximize_window()
+
+    def login_with_credentials(self, username, password):
+        self.driver.find_element(By.ID, "user-name").send_keys(username)
+        self.driver.find_element(By.ID, "password").send_keys(password)
+        self.driver.find_element(By.ID, "login-button").click()
+        time.sleep(2)
+
+    def verify_login_success(self):
+        assert "inventory.html" in self.driver.current_url
+
+    def verify_login_failed(self):
+        error_msg = self.driver.find_element(By.XPATH, "//h3[@data-test='error']").text
+        assert "Username and password do not match any user in this service" in error_msg
+
+    def close_browser(self):
+        self.driver.quit()
